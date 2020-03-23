2ace7e7fc0f0f4c52ef0c05caf259322b8cefd1e
Jardelmc <jardel.casteluber@gmail.com>
2020-03-22 22:47:07 -0300

    youtube-to-mp3 v1.0.0

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://68.183.111.160:3072/api'
+    ? 'https://youtube-to-mp3.netlify.com/api'
     : 'http://192.168.3.110:3072';
 
 const api = axios.create({
