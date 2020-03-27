aaf0c34dc65bd3a5a4c772fd3b9d2a9ee3935cf4
Jardelmc <jardel.casteluber@gmail.com>
2020-03-27 00:59:29 -0300

    fix - url server

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://youtube-to-mp3.netlify.com/api'
+    ? 'https://mp3doyoutube.netlify.com/api'
     : 'http://192.168.3.110:3072';
 
 const api = axios.create({
