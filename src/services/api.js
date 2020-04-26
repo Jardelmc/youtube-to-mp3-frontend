6389c111c89420474872b04d9f1314f087303789
Jardelmc <jardel.casteluber@gmail.com>
2020-04-26 16:36:40 -0300

    fix url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://mp3doyoutube.netlify.com/api'
+    ? 'https://mp3doyoutube.netlify.app/api'
     : 'http://192.168.3.110:3072';
 
 const api = axios.create({
